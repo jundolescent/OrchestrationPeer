1593150ec5aded732203f40669499e22483e7766
jundolescent <houl0310@gmail.com>
2023-11-27 15:11:04 +0900

    fix

--- caliper-benchmarks/startfabcar.sh
+++ caliper-benchmarks/startfabcar.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
  mkdir result
- for i in $(seq 1 2)
+ for i in $(seq 1 5)
  do
   npx caliper launch manager --caliper-workspace ./ --caliper-networkconfig networks/networkConfig.yaml --caliper-benchconfig benchmarks/samples/fabric/fabcar/config.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled
 
