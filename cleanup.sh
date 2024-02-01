238076767aa54f7cbdfe680d6f7e0dd1cdea531f
jundolescent <71925511+jundolescent@users.noreply.github.com>
2024-02-01 13:21:09 +0900

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,7 +2,7 @@
 
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker rmi $(docker images -q)
+docker images | grep 'dev-peer' | awk '{print $3}' | xargs docker rmi -f
 docker volume rm $(docker volume ls -qf dangling=true)
 
 rm -rf ./channel-artifacts
